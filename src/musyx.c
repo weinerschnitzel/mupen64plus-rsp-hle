dc1fa3f62f0315a1cecf70af6e3bbe8c14413931
weinerschnitzel <weinerschnitzel@gmail.com>
2015-12-07 14:35:13 -0700

    Use __int64 instead of int to prevent x64 data loss

--- src/musyx.c
+++ src/musyx.c
@@ -718,7 +718,7 @@ static void mix_voice_samples(struct hle_t* hle, musyx_t *musyx,
     for (i = 0; i < SUBFRAME_SIZE; ++i) {
         /* update sample and lut pointers and then pitch_accu */
         const int16_t *lut = (RESAMPLE_LUT + ((pitch_accu & 0xfc00) >> 8));
-        int dist;
+        __int64 dist;
         int16_t v;
 
         sample += (pitch_accu >> 16);
